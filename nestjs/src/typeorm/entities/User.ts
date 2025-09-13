fd4a084cd7b6ab7ee990accf7241a55fc1b0203f
Thiago <thiagogobb@outlook.com>
2025-09-13 13:12:35 -0300

    update

---
+++ nestjs/src/typeorm/entities/User.ts
@@ -0,0 +1,28 @@
+import { Column, Entity, JoinColumn, OneToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Profile } from "./Profile";
+import { Post } from "./Post";
+
+@Entity({ name: 'users'})
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column()
+  createdAt: Date;
+
+  @Column({ nullable: true})
+  authStrategy: string
+
+  @Column()
+  password: string;
+
+  @OneToOne(()=> Profile)
+  @JoinColumn()
+  profile: Profile;
+
+  @OneToMany(() => Post, (post) => post.user)
+  posts: Post[];
+}
